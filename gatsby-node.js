d6d6eba26d564549a3437d04ac41d3a71a63a20b
Uriel <ur13l.infante@gmail.com>
2021-03-20 14:15:15 -0600

    [UPDATE] Corrigiendo paginación en las entradas de blog para incluir paginador

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,11 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
 const path = require(`path`);
 const slash = require(`slash`);
 
 //Query que devuelve todos los posts y tags
 const queryAllPostsAndTags = async graphql => {
-  const result = await graphql(`
+  const {
+    data: { posts },
+  } = await graphql(`
     query {
       posts {
         listPosts {
@@ -31,7 +26,7 @@ const queryAllPostsAndTags = async graphql => {
       }
     }
   `);
-  return result.data.posts;
+  return posts;
 };
 
 const createAllPostPages = async (posts, createPage, postTemplate) => {
@@ -51,6 +46,145 @@ const createAllPostPages = async (posts, createPage, postTemplate) => {
   });
 };
 
+const createAllBlogPages = async (createPage, template, graphql) => {
+  await graphql(
+    `
+      query somePosts($limit: Int!) {
+        posts {
+          postsByCreatedAt(type: "Post", sortDirection: DESC, limit: $limit) {
+            items {
+              id
+              title
+              content
+              excerpt
+              slug
+              createdAt
+              featured_media
+              tags {
+                items {
+                  tag {
+                    name
+                  }
+                }
+              }
+              authors {
+                items {
+                  author {
+                    firstName
+                    lastName
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    { limit: 1000 }
+  ).then(res => {
+    const { items: posts } = res.data.posts.postsByCreatedAt;
+
+    // Create blog list pages (for Blog List Pagination, BLP)
+    // Assign path /2, /3, p/4, etc
+    const postsPerPage = 3;
+    const numPages = Math.ceil(posts.length / postsPerPage);
+
+    Array.from({ length: numPages }).forEach((_, i) => {
+      // Create blog list pages (for Blog List Pagination, BLP)
+      // Assign path /2, /3, p/4, etc
+      createPage({
+        path: i === 0 ? `/publicaciones/` : `/publicaciones/${i + 1}`,
+        component: slash(template),
+        context: {
+          id: `blog-${i + 1}`,
+          limit: postsPerPage,
+          skip: i * postsPerPage,
+          posts: posts.slice(i * postsPerPage, i * postsPerPage + postsPerPage),
+          numPages,
+          currentPage: i + 1,
+        },
+      });
+    });
+  });
+};
+
+const createAllTagPages = async (tags, createPage, template, graphql) => {
+  const promises = [];
+  tags.forEach(async tag => {
+    promises.push(
+      graphql(
+        `
+          query($id: ID!, $limit: Int!) {
+            posts {
+              postsByTag(tagID: $id, limit: $limit) {
+                items {
+                  post {
+                    id
+                    title
+                    content
+                    excerpt
+                    slug
+                    createdAt
+                    featured_media
+                    tags {
+                      items {
+                        tag {
+                          id
+                          name
+                        }
+                      }
+                    }
+                    authors {
+                      items {
+                        author {
+                          id
+                          firstName
+                          lastName
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `,
+        { limit: 1000, id: tag.id }
+      )
+    );
+
+    return Promise.all(promises).then(results => {
+      results.forEach(res => {
+        const { items: posts } = res.data.posts.postsByTag;
+        const postsPerPage = 2;
+        const numPages = Math.ceil(posts.length / postsPerPage);
+        Array.from({ length: numPages }).forEach((_, i) => {
+          createPage({
+            path:
+              i === 0
+                ? `/publicaciones/${tag.slug}`
+                : `/publicaciones/${tag.slug}/${i + 1}`,
+            component: slash(template),
+            context: {
+              id: `blog-template-${i + 1}`,
+              limit: postsPerPage,
+              skip: i * postsPerPage,
+              posts: posts.slice(
+                i * postsPerPage,
+                i * postsPerPage + postsPerPage
+              ),
+              numPages,
+              tagName: tag.name,
+              slug: tag.slug,
+              currentPage: i + 1,
+            },
+          });
+        });
+      });
+    });
+  });
+};
+
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
@@ -61,97 +195,8 @@ exports.createPages = async ({ graphql, actions }) => {
   const { items: posts } = postsAndTags.listPosts;
   const { items: tags } = postsAndTags.listTags;
   await createAllPostPages(posts, createPage, postTemplate);
-
-  const limit = 10,
-    numPages = Math.ceil(posts.length / limit);
-  let nextToken = null;
-  Array.from({ length: numPages }).forEach(async (_, i) => {
-    const paginatedResults = await graphql(`
-      query {
-        posts {
-          postsByCreatedAt (type:"Post" sortDirection:DESC limit:${limit} nextToken:${
-      nextToken ? `"${nextToken}"` : null
-    }){
-            nextToken
-          }
-        }
-      }
-    `);
-    nextToken = paginatedResults.data.posts.postsByCreatedAt.nextToken;
-
-    createPage({
-      path: i === 0 ? `/publicaciones/` : `/publicaciones/${i + 1}`,
-      component: slash(pageTemplate),
-      context: {
-        id: `blog-${i + 1}`,
-        limit,
-        nextToken: nextToken,
-        numPages,
-        currentPage: i + 1,
-      },
-    });
-  });
-
-  tags.forEach(async tag => {
-    let numPages = 0,
-      _nextToken = null;
-    const { id, name: tagName, slug } = tag,
-      pages = [];
-    do {
-      //do While para que lo haga la primera vez con el nextToken = null
-      const results = await graphql(`
-        query {
-          posts {
-            postsByTag(tagID:"${id}" limit: ${limit} nextToken:${
-        _nextToken ? `"${_nextToken}"` : null
-      }) {
-              nextToken
-              items {
-                id
-              }
-            }
-          }
-        }
-      `);
-      const {
-        data: {
-          posts: {
-            postsByTag: { nextToken, items },
-          },
-        },
-      } = results;
-      _nextToken = nextToken;
-      /* 
-        Cuando el número de elementos totales es divisible entre la variable limit (Ejemplo: 20 elementos y limit=10), se genera iteración 
-        extra, ya que la última página devuelve un nextToken, pero no devuelve items. Esta condición previene que se genere una página extra 
-        sin contenido.  
-      */
-      if (items.length > 0) {
-        numPages++; //Se suma un valor al número de páginas
-        pages.push({ nextToken, currentPage: numPages }); //Se guarda el valor que ayudará en la creación de las páginas posteriormente
-      }
-    } while (_nextToken !== null);
-
-    pages.forEach(async page => {
-      const { currentPage } = page;
-      createPage({
-        path:
-          currentPage === 1
-            ? `publicaciones/${slug}`
-            : `publicaciones/${slug}/${currentPage}`,
-        component: slash(tagTemplate),
-        context: {
-          id,
-          limit,
-          nextToken,
-          numPages,
-          currentPage,
-          tagName,
-          slug,
-        },
-      });
-    });
-  });
+  await createAllBlogPages(createPage, pageTemplate, graphql);
+  return createAllTagPages(tags, createPage, tagTemplate, graphql);
 };
 
 exports.onCreateWebpackConfig = ({ stage, plugins, actions }) => {
